6ccc8ab5b286ec473dd7e8326d4dd98eb1ee00c8
stitch099 <122793174+stitch099@users.noreply.github.com>
2023-01-20 19:54:23 +0200

    tt finished masbaha without local storage

--- public/script.js
+++ public/script.js
@@ -1,10 +1,11 @@
-let count = 0
+let count = 0;
 
-function increase(){
-    count++;
-   ("increase").innerHTML = count;
-};
-function reset(){
+function increase() {
+    count += 1;
+    document.getElementById("count").innerHTML = count;
+}
+
+function reset() {
     count = 0;
-    document.getElementsByClassName("reset").innerHTML = count;
+    document.getElementById("count").innerHTML = count;
 };

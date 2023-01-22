c88648f8d7d64b5fd5636b1d9224d503dc2cedae
stitch099 <122793174+stitch099@users.noreply.github.com>
2023-01-23 00:19:13 +0200

    tt local storage has a bug

--- public/script.js
+++ public/script.js
@@ -1,11 +1,42 @@
 let count = 0;
 
 function increase() {
-    count += 1;
-    document.getElementById("count").innerHTML = count;
+    setValue(count += 1);
 }
 
 function reset() {
-    count = 0;
-    document.getElementById("count").innerHTML = count;
+    setValue(count = 0);
 };
+
+function save() {
+    localStorage.setItem("count", count);
+};
+
+function updateView(){
+    document.querySelector("#count").innerHTML = count;
+}
+// function load() {
+//     localStorage.getItem("count", count);
+    
+// };
+
+// function clear() {
+//     localStorage.clear();
+// };
+
+let setValue = (value) =>{
+    count = value;
+    save();
+    document.getElementById("count").innerHTML = count; 
+};
+
+let main = () =>{
+    let counter = localStorage.getItem("count");
+    if (counter == null){
+        setValue(counter = 0);
+    }else{
+        setValue(counter = parseInt(counter));
+    };
+};
+
+main();
